1e2422787e165e27e8a5ca6063d09d3a203f02f8
Justine <justinecomment@gmail.com>
2017-07-12 23:55:48 +0200

    + Methode GET sur service REST.

--- app/app.module.js
+++ app/app.module.js
@@ -20,50 +20,33 @@ myApp.config(['$routeProvider', function ($routeProvider) {
 }]);
 
 
-myApp.service('contactsService',function($http) {
-  this.jsonContacts = function(){
-    return $http.get('JSON/user.json').success(function(result){
+myApp.service('getAllContacts',function($http, $rootScope, ) {
+  this.getContacts = function(){
+    return $http.get('http://localhost/repertoire/contacts.php').success(function(result){
         return result;
     })
   };
 });
 
-myApp.controller('MainCtrl',function ($scope, contactsService) {
-  contactsService.jsonContacts().then(function(result){
-    $scope.totalEmail = result.data.length;
-  })
+
+myApp.controller('MainCtrl',function ($scope) {
+  
 });
 
-myApp.controller('contactCtrl', function($scope, $resource, $http, $templateCache) {  
-  $scope.method = 'GET';
-  $scope.url='http://localhost/~justinekociszewski/contacts.php';  
-  $scope.fetch = function() {
-      $scope.code = null;
-      $scope.response = null;
-
-      $http({method: $scope.method, url: $scope.url, cache: $templateCache}).
-      success(function(data, status) {
-          $scope.data = data || "Request OK";
-      }).
-      error(function(data, status) {
-          $scope.data = data || "Request failed";
-      });
-  };
-
-  $scope.updateModel = function(method, url) {
-      $scope.method = method;
-      $scope.url = url;
-  };
+myApp.controller('contactCtrl', function($scope,getAllContacts) { 
+   getAllContacts.getContacts().then(function(response) {
+           console.log(response.data)
+            $scope.monContact  = response.data;
+        }),function(error){
+            return error;
+        };
 });
-
-myApp.controller('addCtrl' ,function($scope, $location){
+ 
+myApp.controller('addCtrl' ,function($scope, $http, $location){
   $scope.validateForm = function(){
     if($scope.myAddForm.$valid == true){
-      console.log('valid');   
-      $location.path('/contacts');
+        console.log("valid");
+        $location.path('/contacts');
     }
-    else{
-      console.log('non valid');
-    }
-  };
+  }
 });
